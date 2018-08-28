989739c695119ce0f3098177581abde6e934d340
sercan447 <sercan.goger@gmail.com>
2018-08-29 00:13:41 +0300

    Update TestController.groovy

--- grails-app/controllers/createdate/TestController.groovy
+++ grails-app/controllers/createdate/TestController.groovy
@@ -7,10 +7,16 @@ class TestController {
     @Transactional
     def index() {
         def t = new Test()
-        t.save()
+        //t.save() //do not write it
 
         t.uid = "dfsdf"
+        //write the values I wrote below.try again
+        
+       // t.dateCreated = //give me a date
+        //t.lastUpdated = //give me a date
         t.save()
         render "ok"
+       
     }
 }
+
